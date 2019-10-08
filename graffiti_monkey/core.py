30f530531c778a86663c567d233fd6d2f7b61be8
Paul Canham <paul@canham.io>
2019-10-08 08:39:15 +0100

    making boto3 logging more configurable

--- graffiti_monkey/core.py
+++ graffiti_monkey/core.py
@@ -404,21 +404,30 @@ class Logging(object):
     _log_simple_format = '%(asctime)s [%(levelname)s] %(message)s'
     _log_detailed_format = '%(asctime)s [%(levelname)s] [%(name)s(%(lineno)s):%(funcName)s] %(message)s'
 
-    def configure(self, verbosity = 0):
+    def configure(self, verbosity = int(0)):
         ''' Configure the logging format and verbosity '''
 
         # Configure our logging output
-        if verbosity == 2:
+        if verbosity >= 2:
             logging.basicConfig(level=logging.DEBUG, format=self._log_detailed_format, datefmt='%Y-%m-%d %H:%M:%S')
-        elif verbosity == 1:
+        elif verbosity >= 1:
             logging.basicConfig(level=logging.INFO, format=self._log_detailed_format, datefmt='%Y-%m-%d %H:%M:%S')
         else:
             logging.basicConfig(level=logging.INFO, format=self._log_simple_format, datefmt='%Y-%m-%d %H:%M:%S')
 
         # Configure Boto's logging output
-        if verbosity == 4:
+        if verbosity >= 4:
             logging.getLogger('boto3').setLevel(logging.DEBUG)
-        elif verbosity == 3:
+            logging.getLogger('botocore').setLevel(logging.DEBUG)
+            logging.getLogger('s3transfer').setLevel(logging.DEBUG)
+            logging.getLogger('urllib3').setLevel(logging.DEBUG)
+        elif verbosity >= 3:
             logging.getLogger('boto3').setLevel(logging.INFO)
+            logging.getLogger('botocore').setLevel(logging.INFO)
+            logging.getLogger('s3transfer').setLevel(logging.INFO)
+            logging.getLogger('urllib3').setLevel(logging.INFO)
         else:
             logging.getLogger('boto3').setLevel(logging.CRITICAL)
+            logging.getLogger('botocore').setLevel(logging.CRITICAL)
+            logging.getLogger('s3transfer').setLevel(logging.CRITICAL)
+            logging.getLogger('urllib3').setLevel(logging.CRITICAL)
